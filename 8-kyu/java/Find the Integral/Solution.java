37eadcc427308f4923cfa7dd18af8ac0e67c1141
VladyslavPilkevych <vladyslavpilkevych2004@gmail.com>
2025-02-22 19:22:21 +0100

    8-kyu, Find the Integral, Java

---
+++ 8-kyu/java/Find the Integral/Solution.java
@@ -0,0 +1,9 @@
+public class Kata {
+
+    public static String integrate(int coefficient, int exponent) {
+        int newExponent = exponent + 1;
+        int newCoefficient = coefficient / newExponent;
+        return String.valueOf(newCoefficient) + "x^" + String.valueOf(newExponent);
+    }
+
+}
