c403b0430b4277da591b5d65208c1352eb8fef2e
VladyslavPilkevych <vladyslavpilkevych2004@gmail.com>
2025-02-24 23:29:29 +0100

    refactor code styles

--- 8-kyu/java/Find the Integral/Solution.java
+++ 8-kyu/java/Find the Integral/Solution.java
@@ -1,9 +1,7 @@
 public class Kata {
-
     public static String integrate(int coefficient, int exponent) {
         int newExponent = exponent + 1;
         int newCoefficient = coefficient / newExponent;
         return String.valueOf(newCoefficient) + "x^" + String.valueOf(newExponent);
     }
-
 }
